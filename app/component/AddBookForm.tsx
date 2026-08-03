f774f477053dc13c5d43cd56c30e3e8717eebca4
sameer_first <maliksameer890678@gmail.com>
2026-08-03 19:29:10 +0500

    Add book creation feature with Supabase

---
+++ app/component/AddBookForm.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "../../lib/supabase";
+import { useRouter } from "next/navigation";
+
+export default function AddBookForm() {
+  const router = useRouter();
+
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isbn, setIsbn] = useState("");
+  const [category, setCategory] = useState("");
+  const [publisher, setPublisher] = useState("");
+  const [publicationYear, setPublicationYear] = useState("");
+  const [pages, setPages] = useState("");
+  const [totalCopies, setTotalCopies] = useState("");
+  const [availableCopies, setAvailableCopies] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleAddBook = async () => {
+  const { data, error } = await supabase
+    .from("books")
+    .insert({
+      title,
+      author,
+      isbn,
+      category,
+      publisher,
+      publication_year: Number(publicationYear),
+      pages: Number(pages),
+      total_copies: Number(totalCopies),
+      available_copies: Number(availableCopies),
+    });
+
+    if (error) {
+    console.log(error);
+    setMessage("Failed to add book.");
+    return;
+  }
+
+  setMessage("Book added successfully!");
+  router.refresh(); // Refresh the page to show the new book
+};
+
+  return (
+    <div>
+      <h2>Add New Book</h2>
+
+      <input type="text" placeholder="Title" value={title}
+       onChange={(e) => setTitle(e.target.value)} />
+       
+      <input type="text" placeholder="Author" value={author}
+      onChange={(e) => setAuthor(e.target.value)} />
+
+      <input type="text" placeholder="ISBN" value={isbn}
+      onChange={(e) => setIsbn(e.target.value)} />
+
+      <input type="text" placeholder="Category" value={category}
+      onChange={(e) => setCategory(e.target.value)} />
+
+      <input type="text" placeholder="Publisher" value={publisher}
+      onChange={(e) => setPublisher(e.target.value)} />
+
+      <input type="number" placeholder="Publication Year" value={publicationYear}
+      onChange={(e) => setPublicationYear(e.target.value)} />
+
+      <input type="number" placeholder="Pages" value={pages}
+      onChange={(e) => setPages(e.target.value)} />
+
+      <input type="number" placeholder="Total Copies" value={totalCopies}
+       onChange={(e) => setTotalCopies(e.target.value)}/>
+
+      <input type="number" placeholder="Available Copies" value={availableCopies}
+       onChange={(e) => setAvailableCopies(e.target.value)} />
+
+      <button className="cursor-pointer" onClick={handleAddBook}>Add Book</button>
+      <p>{message}</p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
-export default function Home() {
+import { supabase } from "@/lib/supabase";
+import AddBookForm from "./component/AddBookForm";
+
+export default async function Home() {
+  const { data, error } = await supabase
+    .from("books")
+    .select("*");
+
+  console.log(data);
+  console.log(error);
+
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">
-        Library Management System
-      </h1>
-    </main>
+     <main className="min-h-screen p-10">
+    <h1 className="text-4xl font-bold mb-6">
+      Library Books
+    </h1>
+    <AddBookForm />
+    {data?.map((book) => (
+      <div
+        key={book.id}
+        className="border rounded-lg p-4 mb-4"
+      >
+        <h2 className="text-2xl font-semibold">
+          {book.title}
+        </h2>
+
+        <p>Author: {book.author}</p>
+        <p>ISBN: {book.isbn || "Not Available"}</p>
+        <p>Publisher: {book.publisher || "Not Available"}</p>
+        <p>Category: {book.category}</p>
+        <p>Pages: {book.pages}</p>
+        <p>
+          Available: {book.available_copies} / {book.total_copies}
+        </p>
+      </div>
+    ))}
+  </main>
   );
 }
